610d4798af75d4f41176c710d168351e3fb54d70
LL130385 <LL130385@hnlens.com>
2017-09-20 08:59:00 +0800

    delete frontend

--- xyw.com/FrontEndCode/templates2/project/sbgl/js/sbgl/sbpz/sbpz.js
+++
@@ -1,4 +0,0 @@
-//	controller-----控制器
-app.controller("sbglSbpzContr", ['$scope', 'httpService', '$modal', function ($scope, httpService, $modal) {
-    console.log("设备配置")
-}]);
--- xyw.com/FrontEndCode/智慧教室前端第2版/FrontEndCode/templates2/project/sbgl/js/sbgl/jdpz/jdpz.js
+++
@@ -1,3 +0,0 @@
-app.controller("sbglJdpzContr", ['$scope', 'httpService', '$modal', function ($scope, httpService, $modal) {
-    console.log("节点配置")
-}]);
